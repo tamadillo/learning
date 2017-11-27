844e4e3c7c978d9db33f375d26d3143f607f131f
Matilda <matilda.milewska@gmail.com>
2017-11-27 21:01:50 +1100

    loops

--- python/draw_boxes.py
+++ python/draw_boxes.py
@@ -3,13 +3,8 @@ import turtle
 t = turtle.Pen()
 window = turtle.getscreen()
 
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
+for index in range(0,4):
+    t.forward(100)
+    t.right(90)
 
 window.exitonclick()

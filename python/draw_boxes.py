8b7f0bbe6123bfa1ce3255ec1c49ae096ca6350d
Matilda <matilda.milewska@gmail.com>
2017-11-27 20:59:13 +1100

    my first turtle square

---
+++ python/draw_boxes.py
@@ -0,0 +1,15 @@
+import turtle
+
+t = turtle.Pen()
+window = turtle.getscreen()
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+
+window.exitonclick()

d871e3c386761799860e4bd620b23eb6585a1f23
Matilda <matilda.milewska@gmail.com>
2017-11-27 21:06:47 +1100

    a loop in a loop to do some pictures

--- python/draw_boxes.py
+++ python/draw_boxes.py
@@ -3,8 +3,18 @@ import turtle
 t = turtle.Pen()
 window = turtle.getscreen()
 
-for index in range(0,4):
-    t.forward(100)
+def square():
+    for index in range(0,4):
+        t.forward(100)
+        t.right(90)
+
+for start in range(0,10):
+    square()
+    t.left(90)
+    t.up()
+    t.forward(10)
     t.right(90)
+    t.forward(10)
+    t.down()
 
 window.exitonclick()

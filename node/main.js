ad5d102e589cc9b2e6b39a42b5c977b10ca7f729
Matilda Milewska <tamadillo@users.noreply.github.com>
2019-09-08 17:27:42 +1000

    learning about loops

---
+++ node/main.js
@@ -0,0 +1,18 @@
+console.clear();
+
+const family = [
+  "Matilda",
+  "Michael",
+  "Sarah",
+  "Ambrose",
+  "Felix",
+  "Gracie"
+];
+
+console.log(`hi ${family}`);
+
+for (let number = 0; number < 6; number++) {
+  // console.log(`hi ${family[number]}`);
+
+  console.log(number * 8);
+}
